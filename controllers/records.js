b702fdfc490fa23b4df1375c271fea3ce9f4e760
JasmineKweon <jasmine.j.kweon@gmail.com>
2022-02-04 00:21:06 +0900

    Adding totalIncome/totalSpending

--- controllers/records.js
+++ controllers/records.js
@@ -8,6 +8,7 @@ module.exports.renderCalendar = async(req, res) => {
 }
 
 module.exports.renderIndex = async(req, res) => {
+    /* completed one 
     const year = parseInt(req.query.year);
     const month = parseInt(req.query.month);
     const prevLast = new Date(year, month - 1, 0);
@@ -31,6 +32,40 @@ module.exports.renderIndex = async(req, res) => {
                 path: 'payer',
                 select: 'username'
             })
+        }
+        dates.push(data);
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+    res.render('records/index', { dates });
+    */
+    const year = parseInt(req.query.year);
+    const month = parseInt(req.query.month);
+    const prevLast = new Date(year, month - 1, 0);
+    const thisLast = new Date(year, month, 0);
+    const startDate = new Date(prevLast.setDate(prevLast.getDate() - prevLast.getDay()));
+    const endDate = new Date(thisLast.setDate(thisLast.getDate() + (6 - thisLast.getDay())));
+    let dates = new Array();
+    let currentDate = startDate;
+
+    let i = 1;
+    while (currentDate <= endDate) {
+        const foundRecords = await Record.find({ date: moment(currentDate).format('yyyy-MM-DD') }).populate('category').populate({
+            path: 'payer',
+            select: 'username'
+        })
+
+        if (foundRecords.length !== 0) {
+            console.log(currentDate)
+            console.log(foundRecords)
+            console.log(currentDate)
+        }
+
+        const data = {
+            index: i++,
+            date: new Date(currentDate),
+            totalIncome: "500,000",
+            totalSpending: "700,000",
+            records: foundRecords
         }
         dates.push(data);
         currentDate.setDate(currentDate.getDate() + 1);
